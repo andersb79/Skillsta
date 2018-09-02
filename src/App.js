58aebc8160aa1c94719b5b042bf4a15802c4edd5
Anders Bengtsson <andersb79@hotmail.com>
2018-09-02 13:08:16 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To!! get started, edit <code>src/App.js</code> and save to reload.
-          <Welcome text="hejsan!" toggle={true} />
+          <Welcome text="hejsan!!" toggle={true} />
         </p>
       </div>
     );
